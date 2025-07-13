3f5052bf158ff22c40cb4594914f04f49a1065a7
djamaatul <djamaatul.anbiya@gmail.com>
2025-07-13 12:18:37 +0700

    fix build some code

--- app/(unsignIn)/login/page.tsx
+++ app/(unsignIn)/login/page.tsx
@@ -4,8 +4,8 @@ import Input from "@/app/components/Input";
 export default async function Login() {
   return (
     <form className="flex flex-col gap-4 ">
-      <Input placeholder="Email" />
-      <Input placeholder="Password" />
+      <Input placeholder="Email" name="email" />
+      <Input placeholder="Password" name="password" type="password" />
       <Button type="submit">Login</Button>
 
       <span>
